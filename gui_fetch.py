c17b400eeb5a1646b603b3fd17efae65a6d3a3d7
Fedor Zaytsev <fazajtse@mts.ru>
2017-05-06 12:09:15 +0300

    Updated fetch page design

--- gui_fetch.py
+++ gui_fetch.py
@@ -40,10 +40,13 @@ class FetchPage(tk.Frame):
         self.progress_label = None
         self.estimated_time_box = None
         self.progress_test_label = None
+        self.prev_timedelta = None
+        self.curr_timedelta = None
+        self.estimated_time_coef = 0.002
         self.current_user_idx = 0
-        self.test_task(callback)
+        self.init_controls(callback)
 
-    def test_task(self, callback):
+    def init_controls(self, callback):
         self.directory = filedialog.askdirectory(title='Choose folder with task')
         if self.directory == "":
             return
@@ -54,29 +57,28 @@ class FetchPage(tk.Frame):
 
         callback()
 
-        mainframe = tk.Frame(self)
-        mainframe.pack(side='top', pady=(80, 20))
+        self.columnconfigure(0, weight=1)
+        #self.rowconfigure(0, weight=1)
+        #self.rowconfigure(1, weight=1)
+        #self.rowconfigure(2, weight=1)
+        #self.rowconfigure(3, weight=1)
+        #self.rowconfigure(4, weight=1)
 
-        tk.Label(mainframe, text='Progress:').grid(row=1, column=1)
+        self.progress_label = tk.Label(self, text='Progress:')
+        self.progress_label.grid(row=0, column=0)
 
-        self.progress_bar = ttk.Progressbar(mainframe, length=400)
-        self.progress_bar.grid(row=2, column=1)
+        self.progress_bar = ttk.Progressbar(self, length=400)
+        self.progress_bar.grid(row=1, column=0, sticky='ew')
 
-        self.progress_label = tk.Label(mainframe)
-        self.progress_label.grid(row=2, column=2)
-
-        self.textbox = tk.Label(mainframe)
-        self.textbox.grid(row=3, column=1, pady=(20, 0))
+        self.textbox = tk.Label(self, height=2)
+        self.textbox.grid(row=2, column=0, pady=(20, 0))
         self.textbox.config(text="Preparing for running tests")
 
-        self.progress_test = ttk.Progressbar(mainframe, length=400)
-        self.progress_test.grid(row=4, column=1)
+        self.progress_test = ttk.Progressbar(self, length=400)
+        self.progress_test.grid(row=3, column=0, sticky='ew')
 
-        self.progress_test_label = tk.Label(mainframe, width=10)
-        self.progress_test_label.grid(row=4, column=2)
-
-        self.estimated_time_box = tk.Label(mainframe)
-        self.estimated_time_box.grid(row=5, column=1)
+        self.estimated_time_box = tk.Label(self)
+        self.estimated_time_box.grid(row=4, column=0)
         self.estimated_time_box.config(text="Estimated time: calculating...")
 
         t = threading.Thread(name='fetch.check_folder', target=self.test_task_continue)
@@ -85,20 +87,34 @@ class FetchPage(tk.Frame):
     def step(self, idx, size, name):
         self.current_user_idx = idx
         self.progress_bar['value'] = idx
-        self.progress_bar['maximum'] = size
-        self.textbox.config(text="Testing {}".format(os.path.basename(name['file'])))
+        self.progress_bar['maximum'] = size-1
+        self.textbox.config(text="Testing\n{}".format(os.path.basename(name['file'])))
         self.progress_label.config(text="{}/{}".format(idx + 1, size))
 
     def step_test(self, idx, size, overall_count):
         self.progress_test['value'] = idx
-        self.progress_test['maximum'] = size
-        self.progress_test_label.config(text="{}/{}".format(idx + 1, size))
+        self.progress_test['maximum'] = size-1
+        #self.progress_test_label.config(text="Testing: {}/{}".format(idx + 1, size))
+
+        self.estimated_time_box.config(text=self.calculate_estimated_time(idx, size, overall_count))
+
+    def calculate_estimated_time(self, idx, size, overall_count):
         seconds_passed = (datetime.datetime.now() - self.startTime).total_seconds()
         if seconds_passed >= 10 and idx > 0:
-            time = seconds_passed / (self.current_user_idx*size + idx) * overall_count
+            time = seconds_passed / (self.current_user_idx * size + idx) * overall_count
+            timedelta = datetime.timedelta(seconds=time - seconds_passed)
+            if self.prev_timedelta is None:
+                self.prev_timedelta = timedelta
+            else:
+                self.prev_timedelta = self.curr_timedelta
+
+            print("estimated", timedelta)
+            timedelta = (timedelta - self.prev_timedelta)*self.estimated_time_coef + self.prev_timedelta
+            print("showed", timedelta)
+            self.curr_timedelta = timedelta
 
             self.estimated_time_box.config(
-                text="Estimated time: ~ {}".format(prett_timedelta(datetime.timedelta(seconds=time - seconds_passed))))
+                text="Estimated time: ~ {}".format(prett_timedelta(timedelta)))
 
     def test_task_continue(self):
         self.startTime = datetime.datetime.now()
@@ -106,6 +122,8 @@ class FetchPage(tk.Frame):
         fetch.check_folder(self.directory, self.step, self.step_test, self.main.print_log, self.main.data)
 
         self.main.data.save()
-        messagebox.showinfo(title='Saved', message='Done!')
+        #messagebox.showinfo(title='Saved', message='Done!')
+
+        self.main.open_project()
 
         print("test_task end")
