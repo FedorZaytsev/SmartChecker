6835fec9d60ad509d9fd31dd81e86ac09c7b4dee
Fedor Zaytsev <fazajtse@mts.ru>
2017-05-17 15:46:24 +0300

    Blanked console script

---
+++ main_demon.py
@@ -0,0 +1,11 @@
+import argparse
+import project
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-p', dest='Project file')
+
+
+if __name__ == '__main__':
+    main()
